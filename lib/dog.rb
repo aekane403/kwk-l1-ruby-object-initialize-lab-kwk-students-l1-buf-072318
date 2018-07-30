3e7e9cbd2421c50bdf3e565d9526dd595109f90b
Abigail Kane <aekane403@gmail.com>
2018-07-30 13:18:24 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,8 @@
 # dog.rb
 class Dog
   attr_reader :name,:type, :breed
-  def initialize(name,type= "Pug",breed="Mutt")
+  def initialize(name,breed="Mutt")
     @name = name
-    @type = type
     @breed = breed
   end
 end
